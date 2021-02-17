b39a6ad8207eed8c2fc553abb59fe7d31771732f
Nathalie Flores <21520@student.dosert-college.ie>
2021-02-17 14:37:34 +0000

    third commit

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,12 +3,34 @@ package com.example.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-     Log.i("DEMO", "I am a message")
+        val calculateButton: Button = findViewById(R.id.calculate_button)
+        calculateButton.setOnClickListener{ addition() }
+
+        val num1: EditText = findViewById(R.id.etNum1)
+        val num2: EditText = findViewById(R.id.etNum2)
+        val result: TextView = findViewById(R.id.tvAnswer)
+
+
+
+    }//onCreate
+
+    private fun addition(){
+
+        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        Log.i("DEMO", getString(R.string.Calculate_Message_1))
+
+
+
     }
-}
+}//onMainActivity
